477fe099f6707fed41fabb0dd55d4293c517eacc
Taha <taha.kabul17@gmail.com>
2026-08-12 23:03:50 +0300

    add get_file_info, get_file_content and write_file functions

---
+++ functions/get_files_info.py
@@ -0,0 +1,32 @@
+import os
+
+
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        
+        content = []
+        try:
+            for file in os.listdir(target_dir):
+                file_path = os.path.join(target_dir, file)
+                isdir = os.path.isdir(file_path)
+                size = os.path.getsize(file_path)
+                content.append(f"- {file}: file_size={size} bytes, is_dir={isdir}")
+
+
+            return '\n'.join(content)
+        
+        except Exception as e:  # noqa: BLE001
+            return f"Error: {e}"
+
+    except Exception as e:  # noqa: BLE001
+        return f"Error: {e}"
---
+++ functions/run_python_file.py
@@ -0,0 +1,49 @@
+import os
+import subprocess
+
+
+def run_python_file(
+    working_directory: str, file_path: str, args: list[str] | None = None
+) -> str:
+    try:
+        if not file_path.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file'
+
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+
+        valid_target_dir = (
+            os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        )
+
+        if not valid_target_dir:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(target_dir):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+
+        command = ["python", target_dir]
+        if args:
+            command.extend(args)
+
+        process = subprocess.run(
+            command,
+            cwd=working_dir_abs,
+            capture_output=True,
+            text=True,
+            timeout=30,
+            check=False
+        )
+
+        result = ''
+        result += f"Process exited with code {process.returncode}\n"
+        if not process.stderr and not process.stdout:
+            result += "No output produced\n"
+        else:
+            result += f"STDOUT: {process.stdout}\n"
+            result += f"STDERR: {process.stderr}\n"
+
+        return result
+
+    except Exception as e:  # noqa: BLE001
+        return f"Error: executing Python file: {e}\n"
